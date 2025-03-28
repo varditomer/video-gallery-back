e1c00d76c304500a30f1a47657bcb99e8afdcdb1
varditomer <tomer.vardi@gmail.com>
2025-03-28 12:37:36 +0300

    set server to handle single page apps (always serve the index.html and let the front router to determine the page), set cors and conditionally use it when not in prod.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 // Load environment variables
 dotenv.config();
@@ -9,13 +10,33 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors()); // Enable CORS
+// CORS Configuration
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5173']; // Default React URL
+    app.use(cors({
+        origin: allowedOrigins,
+        credentials: true
+    }))
+}
+
 app.use(express.json()); // Parse JSON requests
 
+// routes
+
 // Health Check Route
-app.get("/alive", (req: Request, res: Response) => {
+app.get("/api/alive", (req: Request, res: Response) => {
   res.status(200).json({ status: "OK", message: "Server is Alive!" });
 });
+
+// Make every server-side-route to match the index.html
+// so when requesting http://localhost:3000/index.html/video/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow react-router to take it from there
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 
 // Start the server
 app.listen(PORT, () => {
